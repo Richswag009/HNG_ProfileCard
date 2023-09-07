63df99b816f7685ed6385a6aaabcc020c9c6b10f
Richswag009 <richesmetelewawon@gmail.com>
2023-09-07 19:32:44 +0100

    change utc time to milliseconds

--- app.js
+++ app.js
@@ -1,30 +1,19 @@
 const currentDay = document.getElementById("currentDayOfTheWeek");
 const currentUTCTime = document.getElementById("currentUTCTime");
-const date = new Date();
-const daysOfWeek = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-currentDay.innerHTML = daysOfWeek[date.getDay()];
 
-const currentUTCTimeInMilliseconds = new Date().getTime();
-const dayTime = new Date(currentUTCTimeInMilliseconds);
-const hours = dayTime.getUTCHours();
-const minutes = dayTime.getUTCMinutes();
-const seconds = dayTime.getUTCSeconds();
-const milliseconds = dayTime.getUTCMilliseconds();
+// const currentUTCTimeInMilliseconds = new Date().getTime();
+// const dayTime = new Date(currentUTCTimeInMilliseconds);
 
-const formattedTime = `${(hours + 1).toString().padStart(2, "0")}:${minutes
-  .toString()
-  .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}:${milliseconds
-  .toString()
-  .padStart(2, "0")}`;
-console.log(formattedTime);
-currentUTCTime.innerHTML = formattedTime;
+// const hours = dayTime.getUTCHours();
+// const minutes = dayTime.getUTCMinutes();
+// const seconds = dayTime.getUTCSeconds();
+// const milliseconds = dayTime.getUTCMilliseconds();
 
-
+// const formattedTime = `${(hours + 1).toString().padStart(2, "0")}:${minutes
+//   .toString()
+//   .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}:${milliseconds
+//   .toString()
+//   .padStart(2, "0")}`;
+// console.log(formattedTime);
+const currentUTCTimeInMilliseconds = Date.now();
+currentUTCTime.innerHTML = currentUTCTimeInMilliseconds;
